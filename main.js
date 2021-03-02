3176d0dbf4e539594521557e4e4fef9f7da4f9b1
Adam Stebbins <acstebbins7@gmail.com>
2021-03-01 21:33:55 -0500

    Finished

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function getFirstItemFrom(arr) {
+  return arr[0]
+}
 
+function getLastItemFrom(arr) {
+  return arr[arr.length-1]
+}
+
+function getIndex3(arr) {
+  if (arr.length >=4) {
+    return arr[3];
+  } else {
+    return arr[arr.length -1];
+  }
+}
+
+function isLongList(arr) {
+  if(arr.length >=10) {
+    return true
+  } else {
+    return false
+  }
+
+}
+
+function firstItemIsNumber(arr) {
+  if(typeof arr[0] === "number") {
+    return true
+  } else
+  return false
+}
+
+function secondCharOfThirdString(arr) {
+  const str3 = arr[2];
+  return str3[1];
+}
 
 
 
